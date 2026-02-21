b1daf1a279ed7366c82cec30fa88792e019314f5
mahmut enes ergul <mahmutergul64@gmail.com>
2026-02-22 02:54:46 +0300

    S13D1 Project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,43 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) {
+            return false;
+        }
+        if (isBarking == true && (clock < 8 || clock > 20)) {
+            return true;
+        }
+        return false;
     }
 
+
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        if ((firstAge >= 13 && firstAge <= 19) || (secondAge >= 13 && secondAge <= 19) || (thirdAge >= 13 && thirdAge <= 19)) {
+            return true;
+        }
+        return false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        int limit = 35;
+        if (isSummer) {
+            limit = 45;
+        }
+        return temp >= 25 && temp <= limit;
     }
 
     public static double area(double width, double height) {
-       
+        if (width < 0 || height < 0) {
+            return -1;
+        }
+        return width * height;
+    }
+    public static double area(double radius) {
+        if (radius < 0) {
+            return -1;
+        }
+        return radius * radius * Math.PI;
     }
 
-    public static double area(double radius) {
-       
-    }
 }
+
